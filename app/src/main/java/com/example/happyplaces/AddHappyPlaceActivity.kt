22905414c43c48bf5be684c69f4513324b4ce19a
Duminda Kuruppu <kuruppu.duminda@gmail.com>
2023-11-11 19:53:33 +0530

    added UI elements

---
+++ app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -0,0 +1,21 @@
+package com.example.happyplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.happyplaces.databinding.ActivityAddHappyPlaceBinding
+
+class AddHappyPlaceActivity : AppCompatActivity() {
+
+    private var binding: ActivityAddHappyPlaceBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setSupportActionBar(binding?.toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding?.toolbarAddPlace?.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+}
